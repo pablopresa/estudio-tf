7d60f43f24760327242427516dee7d7df9ed7821
Pablo Presa <pablo.presa@magentainnova.com>
2024-10-17 01:42:11 -0300

    Se elimina el test-http que fue creado para encapsular el error que se tenía

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SharedModule } from 'primeng/api';
-import { TestHttpComponent } from './test-http/test-http.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [SharedModule, RouterOutlet, TestHttpComponent],
+  imports: [SharedModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
